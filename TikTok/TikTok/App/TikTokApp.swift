996ea9cbac057ca442f09b8111e2f14685fdd6b3
Kalyan <pvkalyan427@gmail.com>
2024-01-31 20:15:20 -0500

    Project Setup

---
+++ TikTok/TikTok/App/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by kalyan . on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
