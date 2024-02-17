28ce3caf5c9ba5d8a42159b61d69a73e3ae81327
Kalyan <pvkalyan427@gmail.com>
2024-02-17 05:50:01 -0500

    Explore View

---
+++ TikTok/TikTok/Core/Search/ExploreView.swift
@@ -0,0 +1,30 @@
+//
+//  ExploreView.swift
+//  TikTok
+//
+//  Created by kalyan . on 2/17/24.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 16) {
+                    ForEach(0 ..< 20) { user in
+                        UserCell()
+                            .padding(.horizontal)
+                    }
+                }
+            }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
+            .padding(.top)
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
---
+++ TikTok/TikTok/Core/Search/UserCell.swift
@@ -0,0 +1,34 @@
+//
+//  UserCell.swift
+//  TikTok
+//
+//  Created by kalyan . on 2/17/24.
+//
+
+import SwiftUI
+
+struct UserCell: View {
+    var body: some View {
+        HStack(spacing: 12) {
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .frame(width: 48, height: 48)
+                .foregroundStyle(Color(.systemGray5))
+            
+            VStack(alignment: .leading) {
+                Text("test_user")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                
+                Text("Test Name")
+                    .font(.footnote)
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    UserCell()
+}
