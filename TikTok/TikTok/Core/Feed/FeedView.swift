4993b1f3b621f7602c8f9b6f507e399cfa2df078
Kalyan <pvkalyan427@gmail.com>
2024-02-17 05:32:34 -0500

    Feed View

---
+++ TikTok/TikTok/Core/Feed/FeedView.swift
@@ -0,0 +1,27 @@
+//
+//  FeedView.swift
+//  TikTok
+//
+//  Created by kalyan . on 2/17/24.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 0) {
+                ForEach(0 ..< 10) { post in
+                    FeedCell(post: post)
+                }
+            }
+            .scrollTargetLayout()
+        }
+        .scrollTargetBehavior(.paging)
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    FeedView()
+}
