bdd9fdf612907f62f14f65056b38f292ff5c762d
Kalyan <pvkalyan427@gmail.com>
2024-02-24 05:39:14 -0500

    Notifications View

---
+++ TikTok/TikTok/Core/Notifications/NotificationsView.swift
@@ -0,0 +1,29 @@
+//
+//  NotificationsView.swift
+//  TikTok
+//
+//  Created by kalyan . on 2/24/24.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 24) {
+                    ForEach(0 ..< 10) { notification in
+                        NotificationCell()
+                    }
+                }
+            }
+            .navigationTitle("Notifications")
+            .navigationBarTitleDisplayMode(.inline)
+            .padding(.top)
+        }
+    }
+}
+
+#Preview {
+    NotificationsView()
+}
