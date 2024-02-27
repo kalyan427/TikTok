73645bca63cc1d39fdc26a212e1e3876486a6052
Kalyan <pvkalyan427@gmail.com>
2024-02-26 22:01:23 -0500

    Displaying Videos

---
+++ TikTok/TikTok/Core/Feed/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  TikTok
+//
+//  Created by kalyan . on 2/26/24.
+//
+
+import Foundation
+
+struct Post: Identifiable, Codable {
+    let id: String
+    let videoUrl: String
+}
---
+++ TikTok/TikTok/Core/Feed/ViewModel/FeedViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  FeedViewModel.swift
+//  TikTok
+//
+//  Created by kalyan . on 2/26/24.
+//
+
+import Foundation
+
+class FeedViewModel: ObservableObject {
+    @Published var posts = [Post]()
+    
+    var videoUrls = [
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
+    ]
+    
+    init() {
+        fetchPosts()
+    }
+    
+    func fetchPosts() {
+        self.posts = [
+            .init(id: NSUUID().uuidString, videoUrl: videoUrls[0]),
+            .init(id: NSUUID().uuidString, videoUrl: videoUrls[1]),
+            .init(id: NSUUID().uuidString, videoUrl: videoUrls[2]),
+            .init(id: NSUUID().uuidString, videoUrl: videoUrls[3])
+        ]
+    }
+}
