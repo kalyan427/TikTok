537aa6a209273016ef3252229c4cd57d07f613c0
Kalyan <pvkalyan427@gmail.com>
2024-04-01 21:22:31 -0400

    Custom video player

--- TikTok/TikTok/Core/Feed/View/FeedCell.swift
+++ TikTok/TikTok/Core/Feed/View/FeedCell.swift
@@ -12,9 +12,9 @@ struct FeedCell: View {
     let post: Post
     var player: AVPlayer
     
-    init(post: Post) {
+    init(post: Post, player: AVPlayer) {
         self.post = post
-        self.player = AVPlayer(url: URL(string: post.videoUrl)!)
+        self.player = player
     }
     
     var body: some View {
@@ -99,11 +99,11 @@ struct FeedCell: View {
             .padding()
         }
         .onAppear {
-            player.play()
+         //   player.play()
         }
     }
 }
 
 #Preview {
-    FeedCell(post: Post(id: NSUUID().uuidString, videoUrl: ""))
+    FeedCell(post: Post(id: NSUUID().uuidString, videoUrl: ""), player: AVPlayer())
 }
