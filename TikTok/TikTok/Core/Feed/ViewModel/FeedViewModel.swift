7bca28e623f5dffadd9fcd5265a71148ad005f6d
Kalyan <pvkalyan427@gmail.com>
2024-04-13 19:23:43 -0400

    Firebase Setup

--- TikTok/TikTok/Core/Feed/ViewModel/FeedViewModel.swift
+++ TikTok/TikTok/Core/Feed/ViewModel/FeedViewModel.swift
@@ -11,10 +11,10 @@ class FeedViewModel: ObservableObject {
     @Published var posts = [Post]()
     
     var videoUrls = [
-        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
-        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
-        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
+        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
     ]
     
     init() {
