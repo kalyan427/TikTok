d8d8c547c3de4ed0b19019f7f3d180c701757dbb
Kalyan <pvkalyan427@gmail.com>
2024-04-13 20:41:04 -0400

    Authentication Service Setup

--- TikTok/TikTok/Core/Authentication/View/LoginView.swift
+++ TikTok/TikTok/Core/Authentication/View/LoginView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct LoginView: View {
     @State private var email = ""
     @State private var password = ""
+    @StateObject var viewModel = LoginViewModel(authService: AuthService())
     
     var body: some View {
         NavigationStack {
@@ -46,7 +47,7 @@ struct LoginView: View {
                 // login button
                 
                 Button {
-                    print("DEBUG: Login")
+                    Task { await viewModel.login(withEmail: email, password: password) }
                 } label: {
                     Text("Login")
                         .font(.subheadline)
